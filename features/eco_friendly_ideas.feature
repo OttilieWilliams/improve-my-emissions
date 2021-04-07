cc44536e26fd22a5e180aa367ae3c03ffdbc6b6e
Ottilie Williams <ottiliewilliams1@sky.com>
2021-04-07 21:17:05 +0100

    Start feature tests

---
+++ features/eco_friendly_ideas.feature
@@ -0,0 +1,5 @@
+Feature: Eco Friendly Ideas
+  Scenario: Adding a eco friendly idea
+    When I visit the add ideas page
+    And I add a new idea
+    Then I see the new idea
---
+++ features/step_definitions/eco_friendly_ideas_steps.rb
@@ -0,0 +1,12 @@
+When('I visit the add ideas page') do
+  visit eco_friendly_ideas_path
+  click_on 'Add Idea'
+end
+
+When('I add a new idea') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see the new idea') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
